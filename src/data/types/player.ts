ac05f304f02f798e9f8572bed8130aa745037e30
adhe223 <adhe223@g.uky.edu>
2019-05-03 20:11:09 -0700

    Add some of the base typings

---
+++ src/data/types/player.ts
@@ -0,0 +1,14 @@
+interface BasePlayer {
+  name: string;
+}
+
+export interface FootballPlayer extends BasePlayer {
+  team: FootballTeam;
+  position: FootballPlayerPosition;
+}
+
+export type PlayerPosition = FootballPlayerPosition;
+export type FootballPlayerPosition = 'QB' | 'RB' | 'WR' | 'TE' | 'D/ST' | 'K' | 'FLEX' | 'OP';
+
+export type PlayerTeam = FootballTeam;
+export type FootballTeam = 'NYG' | 'NYJ';
